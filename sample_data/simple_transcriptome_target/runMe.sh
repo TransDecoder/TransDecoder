fc2643ec99909a669b96e3149c7955dd688516f4
Brian Haas <brianjohnhaas@gmail.com>
2016-02-10 10:40:56 -0500

    add

---
+++ sample_data/simple_transcriptome_target/runMe.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -e Trinity.fasta ]; then
+    gunzip -c Trinity.fasta.gz > Trinity.fasta
+fi
+
+../../TransDecoder.LongOrfs -t Trinity.fasta
+
+../../TransDecoder.Predict -t Trinity.fasta
+
+exit 0
