a8d1791a71e1b4dbdc6e75b7a4e1bf996aa15508
Brian Haas <brianjohnhaas@gmail.com>
2016-02-10 10:37:14 -0500

    add

---
+++ sample_data/pasa_example/cleanme.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use FindBin;
+
+
+## we delete all files we don't need in this directory. Be careful in case users try running it somewhere else, outside this dir.
+chdir $FindBin::Bin or die "error, cannot cd to $FindBin::Bin";
+
+
+
+my @files_to_keep = qw (
+cleanme.pl
+genome.fasta.gz
+pasa_assemblies.fasta.gz
+pasa_assemblies.gff3.gz
+runMe.sh
+                        );
+
+
+my %keep = map { + $_ => 1 } @files_to_keep;
+
+
+foreach my $file (<*>) {
+	
+	if (! $keep{$file}) {
+		print STDERR "-removing file: $file\n";
+		unlink($file);
+	}
+}
+
+
+`rm -rf ./pasa_assemblies.fasta.transdecoder_dir/`;
+
+
+exit(0);
