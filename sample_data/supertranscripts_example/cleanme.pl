6922feceac6ab97801651d8df3cdd0fd1d30f6e4
Brian Haas <brianjohnhaas@gmail.com>
2017-10-12 13:33:05 -0400

    add

---
+++ sample_data/supertranscripts_example/cleanme.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use FindBin;
+
+
+## we delete all files we don't need in this directory. Be careful in case users try running it somewhere else, outside this dir.
+chdir $FindBin::Bin or die "error, cannot cd to $FindBin::Bin";
+
+
+
+my @files_to_keep = qw (cleanme.pl 
+                        runMe.sh
+Makefile
+supertranscripts.fasta 
+supertranscripts.gff3
+                                         );
+
+
+my %keep = map { + $_ => 1 } @files_to_keep;
+
+
+foreach my $file (<*>) {
+	
+	if (! $keep{$file}) {
+		print STDERR "-removing file: $file\n";
+		unlink($file);
+	}
+}
+
+
+`rm -rf ./transcripts.fasta.transdecoder_dir/`;
+`rm -rf ./transcripts.fasta.transdecoder_dir.__checkpoints`;
+
+exit(0);
---
+++ sample_data/supertranscripts_example/runMe.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ev
+
+# convert to gff3 alignment format
+../../util/gtf_to_alignment_gff3.pl supertranscripts.gtf > supertranscripts.gff3
+
+# extract the transcript sequences
+../../util/gtf_genome_to_cdna_fasta.pl supertranscripts.gtf supertranscripts.fasta > transcripts.fasta
+
+# run TransDecoder
+../../TransDecoder.LongOrfs -t transcripts.fasta
+
+../../TransDecoder.Predict -t transcripts.fasta
+
+
+# propagate predicted ORFs to the supertranscript gff3 file:
+../../util/cdna_alignment_orf_to_genome_orf.pl transcripts.fasta.transdecoder.gff3 supertranscripts.gff3 transcripts.fasta > supertranscripts.wOrfs.gff3
+
+# make bed file for visualization
+../../util/gff3_file_to_bed.pl supertranscripts.wOrfs.gff3 > supertranscripts.wOrfs.bed
+
+
+
